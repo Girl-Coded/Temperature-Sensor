d22df9c9da98ca6b4090e79d58f32ce6e222b4e2
Fatima Kammona <fqkammona@gmail.com>
2023-09-19 09:26:58 -0500

    testing.py now continsistently reads in data from the while loop.

--- testing.py
+++ testing.py
@@ -7,8 +7,13 @@ ds_pin = machine.Pin(17)
 ds = ds18x20.DS18X20(onewire.OneWire(ds_pin))
 roms = ds.scan()[0]  # the one and only sensor
 print('Found DS devices: ', roms)
-ds.convert_temp()
-time.sleep_ms(750)
-temp_cel = ds.read_temp(roms)
-temp_fah = ds.get_fahrenheit(temp_cel)
-print("Temp: {}°C {}°F".format(temp_cel,temp_fah)) 
+
+while True:
+    ds.convert_temp()
+    time.sleep_ms(750)
+    for rom in roms:
+        temp_cel = ds.read_temp(roms)
+        temp_fah = ds.get_fahrenheit(temp_cel)
+        print("Temp: {}°C {}°F".format(temp_cel,temp_fah))
+        time.sleep(2)
+    time.sleep(1)
